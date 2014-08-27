250cc13216accc62449504d43e058a402c444abc
Imran Peerbhai <ipeerbhai@github.com>
2014-08-26 19:39:52 -0700

    Uodated text a bit.  Idea is to have it speak this while moving.

--- SpeachPart/SpeachPart/Program.cs
+++ SpeachPart/SpeachPart/Program.cs
@@ -9,12 +9,23 @@ namespace SpeachPart
 {
     class Program
     {
+        static void RunDriveActions()
+        {
+            // do nothing.
+        }
         static void Main(string[] args)
         {
+            RunDriveActions(); // Drive to the cooking person.
             System.Speech.Synthesis.SpeechSynthesizer mySynth = new System.Speech.Synthesis.SpeechSynthesizer();
+            var Voices = mySynth.GetInstalledVoices();
+            mySynth.SelectVoice(Voices[2].VoiceInfo.Name);
+            /*
+
             mySynth.Speak("Step 1. Cut one carrot into slices.");
             mySynth.Speak("Step 2. Put the carrot slices into the pot of water.");
             mySynth.Speak("Step 3. Boil the water.");
+             * */
+            mySynth.Speak("Warning, small robot in motion.  Please look down.");
         }
     }
 }
