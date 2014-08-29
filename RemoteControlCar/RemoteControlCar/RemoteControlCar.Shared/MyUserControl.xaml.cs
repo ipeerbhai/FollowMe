92996882f89f6673d849c14bd975b4581a99365b
Imran Peerbhai <ipeerbhai@aol.com>
2014-08-28 22:14:22 -0700

    adding the remote control car code( the joysticks to drive the rover)

---
+++ RemoteControlCar/RemoteControlCar/RemoteControlCar.Shared/MyUserControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace RemoteControlCar
+{
+    public sealed partial class MyUserControl : UserControl
+    {
+        public double m_positionLastY = 0;
+        public int PercentPower
+        {
+            get
+            {
+                return ((int)m_positionLastY);
+            }
+        }
+        public MyUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Ellipse_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            txtOutput.Text = e.Cumulative.Translation.Y.ToString();
+            if (e.Cumulative.Translation.Y < -100)
+            {
+                yTranslator.Y = -100.0;
+                m_positionLastY = 100.0;
+
+            }
+            else if (e.Cumulative.Translation.Y > 100)
+            {
+                yTranslator.Y = 100.0;
+                m_positionLastY = -100.0;
+
+            }
+            else
+            {
+                yTranslator.Y = e.Cumulative.Translation.Y;
+                m_positionLastY = -yTranslator.Y;
+
+            }
+
+        }
+
+        private void Ellipse_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void Ellipse_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            yTranslator.Y = 0.0;
+        }
+    }
+}
---
+++ RemoteControlCar/RemoteControlCar/RemoteControlCar.Windows/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RemoteControlCar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private int m_LeftMotor;
+        private int m_RightMotor;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Windows.Graphics.Display.DisplayInformation.AutoRotationPreferences = Windows.Graphics.Display.DisplayOrientations.Landscape;
+        }
+
+        private void joystickLeft_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string foo;
+            MyUserControl Joystick = (MyUserControl)sender;
+            int y = Joystick.PercentPower;
+            foo = e.OriginalSource.ToString();
+        }
+
+        private string MakeStringPercent(int inputValue)
+        {
+            string returnMe = "";
+            if (inputValue >= 0)
+            {
+                returnMe = "+" + inputValue.ToString("D3");
+            }
+            else
+            {
+                returnMe = inputValue.ToString("D3");
+            }
+            return (returnMe);
+        }
+
+        private void joystick_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            MyUserControl Joystick = (MyUserControl)sender;
+            string joystickInfo = "";
+            // figure out if I'm left or right...
+            if (Joystick.Name.Contains("Left"))
+            {
+                m_LeftMotor = Joystick.PercentPower;
+            }
+            else
+            {
+                m_RightMotor = Joystick.PercentPower;
+            }
+            // Build the string we send..
+            joystickInfo = MakeStringPercent(m_LeftMotor);
+            joystickInfo += MakeStringPercent(m_RightMotor);
+
+            // Call the Azure class.
+            //shazbot.TodoItem myItem = new shazbot.TodoItem();
+            //shazbot.TodoItem tdi = new shazbot.TodoItem();
+            shazbot.TodoItem AzureConnect = new shazbot.TodoItem();
+            AzureConnect.SendString2(joystickInfo);
+        }
+
+        private void joystickRight_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            MyUserControl Joystick = (MyUserControl)sender;
+            string joystickInfo = "";
+            // figure out if I'm left or right...
+            if (Joystick.Name.Contains("Left"))
+            {
+                m_LeftMotor = Joystick.PercentPower;
+            }
+            else
+            {
+                m_RightMotor = Joystick.PercentPower;
+            }
+            // Build the string we send..
+            joystickInfo = MakeStringPercent(m_LeftMotor);
+            joystickInfo += MakeStringPercent(m_RightMotor);
+
+            // Call the Azure class.
+            //shazbot.TodoItem myItem = new shazbot.TodoItem();
+            //shazbot.TodoItem tdi = new shazbot.TodoItem();
+            shazbot.TodoItem AzureConnect = new shazbot.TodoItem();
+            AzureConnect.SendString2(joystickInfo);
+
+        }
+    }
+}
